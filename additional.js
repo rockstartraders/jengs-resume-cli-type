e9e74c945ea25d3227fca8b80429547a5eb699d6
rockstartraders <32810135+rockstartraders@users.noreply.github.com>
2019-03-24 20:13:14 +0800

    initial commit and upload

---
+++ additional.js
@@ -0,0 +1,29 @@
+/* function to remove inspect elements  */
+/* I used this as reference https://codepen.io/Nielssg/pen/pbroPW  */
+
+
+document.addEventListener('keydown', function() {
+if (event.keyCode == 123) {
+ alert("Ooops. I'm so sorry, But I decided to disable this function.");
+  window.event.returnValue = false;
+} else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+alert("Ooops. I'm so sorry, But I decided to disable this function.");
+
+window.event.returnValue = false;
+} else if (event.ctrlKey && event.keyCode == 85) {
+ alert("Ooops. I'm so sorry, But I decided to disable this function.");
+ window.event.returnValue = false;
+}
+}, false);
+
+if (document.addEventListener) {
+document.addEventListener('contextmenu', function(e) {
+alert("Ooops. I'm so sorry, But I decided to disable this function.");
+  e.preventDefault();
+}, false);
+} else {
+document.attachEvent('oncontextmenu', function() {
+alert("Ooops. I'm so sorry, But I decided to disable this function.");
+  window.event.returnValue = false;
+});
+}
---
+++ terminal.js
@@ -0,0 +1,277 @@
+﻿$(function() {
+
+    $('.prompt').html('root@James_is_my_name:~#');
+
+  var term = new Terminal('#input-line .cmdline', '#container output');
+  term.init();
+
+});
+
+var util = util || {};
+util.toArray = function(list) {
+  return Array.prototype.slice.call(list || [], 0);
+};
+
+var Terminal = Terminal || function(cmdLineContainer, outputContainer) {
+  window.URL = window.URL || window.webkitURL;
+  window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+
+  var cmdLine_ = document.querySelector(cmdLineContainer);
+  var output_ = document.querySelector(outputContainer);
+
+  const CMDS_ = [
+    'whoami', 'education', 'security', 'programming', 'interests', 'love', 'media' ,'contact', 'clear', 'help'
+  ];
+
+  var fs_ = null;
+  var cwd_ = null;
+  var history_ = [];
+  var histpos_ = 0;
+  var histtemp_ = 0;
+
+  window.addEventListener('click', function(e) {
+    cmdLine_.focus();
+  }, false);
+
+  cmdLine_.addEventListener('click', inputTextClick_, false);
+  cmdLine_.addEventListener('keydown', historyHandler_, false);
+  cmdLine_.addEventListener('keydown', processNewCommand_, false);
+
+  //
+  function inputTextClick_(e) {
+    this.value = this.value;
+  }
+
+  //
+  function historyHandler_(e) {
+    if (history_.length) {
+      if (e.keyCode == 38 || e.keyCode == 40) {
+        if (history_[histpos_]) {
+          history_[histpos_] = this.value;
+        } else {
+          histtemp_ = this.value;
+        }
+      }
+
+      if (e.keyCode == 38) { // up
+        histpos_--;
+        if (histpos_ < 0) {
+          histpos_ = 0;
+        }
+      } else if (e.keyCode == 40) { // down
+        histpos_++;
+        if (histpos_ > history_.length) {
+          histpos_ = history_.length;
+        }
+      }
+
+      if (e.keyCode == 38 || e.keyCode == 40) {
+        this.value = history_[histpos_] ? history_[histpos_] : histtemp_;
+        this.value = this.value; // Sets cursor to end of input.
+      }
+    }
+  }
+
+  //
+  function processNewCommand_(e) {
+
+    if (e.keyCode == 9) { // tab
+      e.preventDefault();
+      // Implement tab suggest.
+    } else if (e.keyCode == 13) { // enter
+      // Save shell history.
+      if (this.value) {
+        history_[history_.length] = this.value;
+        histpos_ = history_.length;
+      }
+
+      // Duplicate current input and append to output section.
+      var line = this.parentNode.parentNode.cloneNode(true);
+      line.removeAttribute('id')
+      line.classList.add('line');
+      var input = line.querySelector('input.cmdline');
+      input.autofocus = false;
+      input.readOnly = true;
+      output_.appendChild(line);
+
+      if (this.value && this.value.trim()) {
+        var args = this.value.split(' ').filter(function(val, i) {
+          return val;
+        });
+        var cmd = args[0].toLowerCase();
+        args = args.splice(1); // Remove cmd from arg list.
+      }
+
+      switch (cmd) {
+        case 'clear':
+          output_.innerHTML = '';
+          this.value = '' ;
+          return output('');
+
+
+        case 'help':
+          var result = "<h2>Help</h2><p><b>whoami</b>: display all my information.<br><b>education</b>: display all my information about my education.<br><b>programming</b>: display all my information about my achievements in programming.<br><b>interests</b>: display all my interests.<br><b>contact</b>: Say hi<br><b>clear</b>: clear terminal<br><b>help</b>: display this menu.</p>";
+          output(result);
+          break;
+
+        case 'education':
+          var result = "<h3>Education:</h3>";
+          output(result);
+          break;
+
+        case 'programming':
+          var result = "<h3>Competitive Programmer</h3>";
+          output(result);
+          break;
+
+        case 'interests':
+          var result = "<h3>Interests</h3>";
+          output(result);
+          break;
+
+        case 'contact':
+          var result = "<h3>Contact</h3>";
+          var result1 = "<h3>Contact</h3>";
+          var result2 = "<h3>Contact</h3>";
+          output(result + result1 + result2);
+          break;
+
+        case 'whoami':
+          var result = "<h1>James Paul Espeña</h1>";
+          output(result);
+          break;
+
+		    case 'github':
+          var result = window.open('https://github.com/rockstartraders');
+		      output(" Thank You So Much For Visiting My Github Page." + "<br>" + "Hope You Like It.");
+		    break;
+
+        case 'time':
+          var result = currentDate = new Date();
+          var resulta = "<p>The Date and Time as well as your Timezone is : </p>";
+		      output(resulta + result);
+		    break;
+
+        case 'resume':
+          var result = "<p>My apoligies but I\'m not comfortable in providing my personal information specially online.</p>";
+          var resultb = "<p>But please feel free to email or contact me by typing the command \t'contact \t'.</p>";
+          var resultbb = "<p>And I will be glad to respond back to you and have my resume sent out when that happens.</p>";
+          output(result + resultb + resultbb);
+        break;
+
+        default:
+          if (cmd) {
+            output(cmd + ': command not found ' + '!' + "<br>" +  "I Cannot Recall Creating a Command Like That." + "<br>" + " Please Type 'Help' or 'help' for Further Assistance.");
+          }
+      };
+
+      window.scrollTo(0, getDocHeight_());
+      this.value = ''; // Clear/setup line for next input.
+    }
+  }
+
+
+  //
+  function formatColumns_(entries) {
+    var maxName = entries[0].name;
+    util.toArray(entries).forEach(function(entry, i) {
+      if (entry.name.length > maxName.length) {
+        maxName = entry.name;
+      }
+    });
+
+    var height = entries.length <= 3 ?
+        'height: ' + (entries.length * 15) + 'px;' : '';
+
+    // 12px monospace font yields ~7px screen width.
+    var colWidth = maxName.length * 7;
+
+    return ['<div class="ls-files" style="-webkit-column-width:',
+            colWidth, 'px;', height, '">'];
+  }
+
+  //
+  function output(html) {
+    output_.insertAdjacentHTML('beforeEnd', '<p>' + html + '</p>');
+  }
+
+  // Cross-browser impl to get document's height.
+  function getDocHeight_() {
+    var d = document;
+    return Math.max(
+        Math.max(d.body.scrollHeight, d.documentElement.scrollHeight),
+        Math.max(d.body.offsetHeight, d.documentElement.offsetHeight),
+        Math.max(d.body.clientHeight, d.documentElement.clientHeight)
+    );
+  }
+
+  //
+  return {
+    init: function() {
+      output('');
+
+    },
+    output: output
+  }
+};
+
+
+
+// type writer effect starts here  //
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
