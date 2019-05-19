a2c91cd25beeb1a360f37d58fdb43db9e3075d77
rockstartraders <32810135+rockstartraders@users.noreply.github.com>
2019-05-20 02:39:18 +0800

    fix jquery para pilak

--- js/terminal.js
+++ js/terminal.js
@@ -245,11 +245,12 @@ var Terminal = Terminal || function(cmdLineContainer, outputContainer) {
            break;
 
            case 'pinilakang-tabing':
-           var game1 = '<br><br><center><div class="embed-container"><iframe width="100%" height="700" src="http://wek.ovh/"></iframe></div></center>';
-           output( game1 );
+           var pilak = $('output').html('<object id="pilak" width="100%" height="700" data="http://wek.ovh/"/>');
+           var pilak2 = '<br><center>Just Have Fun</center><br><br>';
+           output(pilak2);
            break;
 
- 
+
 
 
 
